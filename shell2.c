52ef5512d185b83d71b2d15c23af642d0ca2af73
harper71 <princeazuka99@gmail.com>
2024-02-28 00:30:34 +0100

    shell2.c for task 2 commit(1)

---
+++ shell2.c
@@ -0,0 +1,91 @@
+#include "main.h"
+/**
+ * main - used to create a command shell enviroment
+ * @ac: argument count
+ * @av: arguments
+ *
+ * Return: 0 always
+ */
+int main(int ac, char *av[])
+{
+	char *prompt = NULL, *str_cpy = NULL;
+
+	char *delm = " \n";
+
+	size_t lenVar = 0;
+
+	int var, len_prompt, i, argc;
+
+	char *token = NULL, **argv = NULL;
+
+	pid_t pid;
+
+	printf("$ ");
+	while (getline(&prompt, &lenVar, stdin) != EOF)
+	{
+		len_prompt = strlen(prompt);
+
+		prompt[len_prompt - 1] = '\0';
+
+		str_cpy = strdup(prompt);
+
+		argc = 0;
+
+		token = strtok(prompt, delm);
+
+		while (token)
+		{
+			token = strtok(NULL, delm);
+
+			argc++;
+		}
+		argv = malloc(sizeof(char *) * (argc + 1));
+
+		if (argv == NULL)
+		{
+			perror("malloc");
+			exit(EXIT_FAILURE);
+		}
+
+		token = strtok(str_cpy, delm);
+
+		i = 0;
+
+		while (token)
+		{
+			argv[i] = token;
+
+			token = strtok(NULL, delm);
+			i++;
+		}
+		argv[i] = NULL;
+
+		pid = fork();
+		if (pid == -1)
+		{
+			perror("can't create the child process");
+			exit(EXIT_FAILURE);
+		}
+		else if (pid == 0)
+		{
+			var = execve(argv[0], argv, NULL);
+
+			if (var == -1)
+			{
+				printf("%s: no such directory\n", av[0]);
+				exit(EXIT_FAILURE);
+			}
+		}
+		else
+		{
+			wait(NULL);
+		}
+		printf("$ ");
+		free(argv);
+		free(str_cpy);
+	}
+	(void)ac;
+	free(prompt);
+	return (0);
+}
+
